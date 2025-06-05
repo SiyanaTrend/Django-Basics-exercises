6bc3ae0ff6d6a107da473ecfcccbb8698da32572
Siyana Trendafilova <bebovska@gmail.com>
2025-06-06 02:46:20 +0300

    feat: Create examples in views.py

--- djangoIntroduction/tasks/views.py
+++ djangoIntroduction/tasks/views.py
@@ -1,3 +1,32 @@
+from django.http import HttpResponse
+
 from django.shortcuts import render
 
-# Create your views here.
+from tasks.models import Task
+
+
+def index(request):
+    search_title = request.GET.get('filter_title', '')
+
+    tasks = Task.objects.filter(name__icontains=search_title)
+
+    context = {
+        'search_title': search_title,
+        'tasks': tasks,
+    }
+    return render(request, 'index.html', context)
+
+
+"""first example:"""
+# def index(request):
+#     tasks = Task.objects.all()
+#     output = '\n'.join([
+#         '<h1>TASKS</h1>',
+#         '<ul>',
+#         *[f"<li>{t.name}: {t.description}" for t in tasks],
+#         '</ul>'
+#     ])
+#     if not output:
+#         output = 'There are no created tasks!'
+#
+#     return HttpResponse(output)
