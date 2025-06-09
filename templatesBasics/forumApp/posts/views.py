fbefbbc6b9912b4716f054260f700703bce45608
Siyana Trendafilova <bebovska@gmail.com>
2025-06-10 01:17:37 +0300

    feat: Add more solutions

--- templatesBasics/forumApp/posts/views.py
+++ templatesBasics/forumApp/posts/views.py
@@ -1,5 +1,27 @@
+from datetime import datetime
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
+
+# def index(request):
+#     return HttpResponse(f"Welcome to the forum app!")
+
 def index(request):
-    return HttpResponse(f"Welcome to the forum app!")
+    context = {
+        "current_time": datetime.now(),
+        "person": {
+            "age": 20,
+            "height": 1.90,
+        },
+        "IDs": ["1223", "exe1234", "si5684"],
+        "some_text": "everything works well!",
+        "no_text": "",
+        "users": ["pesho", "ivan", "stamat", "maria", "magdalena"]
+    }
+    return render(request, 'base.html', context)
+
+
+def dashboard(request):
+    return render(request, 'base.html')
