24c29e58ef5cf081ca59fbdf3983450634d01834
Siyana Trendafilova <bebovska@gmail.com>
2025-06-15 22:13:21 +0300

    chore: Update forms.py

--- formsBasics/forumApp/posts/forms.py
+++ formsBasics/forumApp/posts/forms.py
@@ -1,30 +1,80 @@
 from django import forms
 
+from posts.models import Post
 
-class MyFrom(forms.Form):
-    CHOICES = [
-        ('1', 'option 1'),
-        ('2', 'option 2'),
-        ('3', 'option 3'),
-    ]
 
-    my_name = forms.CharField(
-        max_length=10,
-        required=False,
-        # initial='Hello',
-        label="Put the info:",
-        help_text="Put your name",
-        widget=forms.Textarea(
-            attrs={'cols': 80,
-                   'class': 'search-bar',
-                   'placeholder': 'Enter the information here!'}
-        )
+class PostForm(forms.ModelForm):
+    lecturer = forms.BooleanField(
+        required=True,
     )
+    class Meta:
+        model = Post
+        fields = '__all__'
+        # fields = ('title', 'content')
+        # exclude = ['content']
+        # widgets = {
+        #     'title': forms.NumberInput,
+        # }
+        # help_texts = {
+        #     'title': 'Put a title'
+        # }
+        # labels = {
+        #     'title': 'This is title'
+        # }
+        # error_messages = {
+        #     'title': {
+        #         'required': '',
+        #     }
+        # }
+        widgets = {
+            'language': forms.RadioSelect(
+                attrs={'class': 'radio-select'},
+            )
+        }
 
-    password = forms.CharField(
-        required=False,
-        widget=forms.PasswordInput(),
-    )
+# class PostForm(forms.ModelForm) instead of =>
+# class PostForm(forms.Form):
+#     title = forms.CharField(
+#         max_length=100,
+#     )
+#     content = forms.CharField(
+#         widget=forms.Textarea,
+#     )
+#     author = forms.CharField(
+#         max_length=30,
+#     )
+#     created_at = forms.DateTimeField()
+#     languages = forms.ChoiceField(
+#         choices=LanguageChoice.choices,
+#     )
+
+
+# Examples:
+# class MyFrom(forms.Form):
+#     CHOICES = [
+#         ('1', 'option 1'),
+#         ('2', 'option 2'),
+#         ('3', 'option 3'),
+#     ]
+#
+#     my_name = forms.CharField(
+#         max_length=10,
+#         required=False,
+#         # initial='Hello',
+#         label="Put the info:",
+#         help_text="Put your name",
+#         widget=forms.Textarea(
+#             attrs={'cols': 40,
+#                    'rows': 5,
+#                    'class': 'search-bar',
+#                    'placeholder': 'Enter the information here!'}
+#         )
+#     )
+
+    # password = forms.CharField(
+    #     required=False,
+    #     widget=forms.PasswordInput(),
+    # )
 
     # my_text = forms.CharField(
     #     widget=forms.Textarea()
