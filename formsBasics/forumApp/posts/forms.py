e33467253bb6a0826ec33a5d1b1671b6e885aa92
Siyana Trendafilova <bebovska@gmail.com>
2025-06-15 20:10:12 +0300

    chore: Add more examples for form fields/widgets

--- formsBasics/forumApp/posts/forms.py
+++ formsBasics/forumApp/posts/forms.py
@@ -2,6 +2,52 @@ from django import forms
 
 
 class MyFrom(forms.Form):
-    my_text = forms.CharField(
+    CHOICES = [
+        ('1', 'option 1'),
+        ('2', 'option 2'),
+        ('3', 'option 3'),
+    ]
+
+    my_name = forms.CharField(
         max_length=10,
+        required=False,
+        # initial='Hello',
+        label="Put the info:",
+        help_text="Put your name",
+        widget=forms.Textarea(
+            attrs={'cols': 80,
+                   'class': 'search-bar',
+                   'placeholder': 'Enter the information here!'}
+        )
     )
+
+    password = forms.CharField(
+        required=False,
+        widget=forms.PasswordInput(),
+    )
+
+    # my_text = forms.CharField(
+    #     widget=forms.Textarea()
+    # )
+    #
+    # multiple_select = forms.MultipleChoiceField(
+    #     choices=CHOICES,
+    #     widget=forms.CheckboxSelectMultiple,
+    # )
+    #
+    # checkbox_field = forms.BooleanField(
+    #     required=False,
+    # )
+    #
+    # radio_field = forms.ChoiceField(
+    #     choices=CHOICES,
+    #     widget=forms.RadioSelect(),
+    # )
+    #
+    # radio_box = forms.CharField(
+    #     widget=forms.RadioSelect(choices=CHOICES),
+    # )
+    #
+    # date = forms.DateField(
+    #     widget=forms.SelectDateWidget(),
+    # )
