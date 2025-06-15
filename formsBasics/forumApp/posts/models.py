f86547a29c3d16f56fb12f3d33b8ead26e67c6de
Siyana Trendafilova <bebovska@gmail.com>
2025-06-15 22:13:00 +0300

    feat: Create post model

--- formsBasics/forumApp/posts/models.py
+++ formsBasics/forumApp/posts/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+from posts.choices import LanguageChoices
+from posts.validators import BadLanguageValidator
+
+
+class Post(models.Model):
+    title = models.CharField(
+        max_length=100,
+    )
+    content = models.TextField(
+        validators=[
+            BadLanguageValidator(),
+        ]
+    )
+    author = models.CharField(
+        max_length=50,
+    )
+    created_at = models.DateField(
+        auto_now_add=True,
+    )
+    language = models.CharField(
+        max_length=20,
+        choices=LanguageChoices.choices,
+        default=LanguageChoices.OTHER,
+    )
