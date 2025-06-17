aff3dac119a39ac807e5b8ac77c6c896c3981da4
Siyana Trendafilova <bebovska@gmail.com>
2025-06-18 00:17:43 +0300

    feat: Create examples with crispy forms  for creating posts - forms.py, add-post.html, post-details.html

--- formsAdvanced/forumApp/forumApp/settings.py
+++ formsAdvanced/forumApp/forumApp/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "posts.apps.PostsConfig"
+    "posts.apps.PostsConfig",
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
 
 MIDDLEWARE = [
@@ -128,3 +130,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
