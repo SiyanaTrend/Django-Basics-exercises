e821ecb737ffefa06fcb232a971b5699423b0769
Siyana Trendafilova <bebovska@gmail.com>
2025-06-17 22:27:09 +0300

    feat: Create comment model

--- formsAdvanced/forumApp/posts/models.py
+++ formsAdvanced/forumApp/posts/models.py
@@ -24,3 +24,21 @@ class Post(models.Model):
         choices=LanguageChoices.choices,
         default=LanguageChoices.OTHER,
     )
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments',
+    )
+
+    author = models.CharField(
+        max_length=50,
+    )
+
+    content = models.TextField()
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
