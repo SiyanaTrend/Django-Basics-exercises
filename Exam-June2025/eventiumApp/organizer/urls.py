4c6916dd00c2ec1433227a8049fa1220b724b99c
Siyana Trendafilova <bebovska@gmail.com>
2025-07-06 22:12:53 +0300

    feat: Add organizer app functionality

--- Exam-June2025/eventiumApp/organizer/urls.py
+++ Exam-June2025/eventiumApp/organizer/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from organizer.views import CreateOrganizerView
+from organizer.views import CreateOrganizerView, OrganizerDetailsView
 
 urlpatterns = [
     path('create/', CreateOrganizerView.as_view(), name='create-organizer'),
+    path('profile/', OrganizerDetailsView.as_view(), name='organizer-details'),
 ]
--- Exam-June2025/eventiumApp/organizer/views.py
+++ Exam-June2025/eventiumApp/organizer/views.py
@@ -16,3 +16,16 @@ class CreateOrganizerView(CreateView):
         response = super().form_valid(form)
         return response
 
+class OrganizerDetailsView(DetailView):
+    model = Organizer
+    template_name = 'organizer/details-organizer.html'
+    context_object_name = 'organizer'
+
+    def get_object(self, queryset=None):
+        return Organizer.objects.first()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        upcoming_events = self.object.events.filter(start_time__gt=timezone.now()).order_by('start_time')
+        context['upcoming_events'] = upcoming_events
+        return context
