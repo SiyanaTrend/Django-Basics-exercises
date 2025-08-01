35b427c5cc382af4f5cb6fe0ef3c2cc77b001f20
Siyana Trendafilova <bebovska@gmail.com>
2025-08-01 21:33:57 +0300

    feat: Add homepage functionality - with or no profile created

--- Exam-Preparation/myMusicApp/common/views.py
+++ Exam-Preparation/myMusicApp/common/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, FormView
 
-# Create your views here.
+from albums.models import Album
+from common.utils import get_profile
+from profiles.forms import ProfileCreateForm
+
+
+class HomePage(ListView, FormView):
+    model = Album
+    form_class = ProfileCreateForm
+    success_url = reverse_lazy('home')
+
+    def get_template_names(self):
+        profile = get_profile()  # None or QuerySet utils.py
+
+        if profile:
+            return ['profiles/home-with-profile.html']
+
+        return ['profiles/home-no-profile.html']
+
+    def form_valid(self, form):
+        if form.is_valid:
+            form.save()
+            return super().form_valid(form)
