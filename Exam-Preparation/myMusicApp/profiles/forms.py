260082af18af836f9068134f324168120665a824
Siyana Trendafilova <bebovska@gmail.com>
2025-08-02 20:45:23 +0300

    feat: Add details/delete profile functionality

--- Exam-Preparation/myMusicApp/profiles/forms.py
+++ Exam-Preparation/myMusicApp/profiles/forms.py
@@ -8,6 +8,18 @@ class ProfileBaseForm(forms.ModelForm):
         model = Profile
         fields = "__all__"
 
+        widgets = {
+            'username': forms.TextInput(
+                attrs={'placeholder': 'Username'}
+            ),
+            'email': forms.EmailInput(
+                attrs={'placeholder': 'Email'}
+            ),
+            'age': forms.NumberInput(
+                attrs={'placeholder': 'Age'}
+            ),
+        }
+
 
 class ProfileCreateForm(ProfileBaseForm):
     pass
--- Exam-Preparation/myMusicApp/profiles/views.py
+++ Exam-Preparation/myMusicApp/profiles/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from typing import Optional
 
-# Create your views here.
+from django.db.models import QuerySet
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, DeleteView
+
+from common.utils import get_profile
+from profiles.models import Profile
+
+
+class ProfileDetailView(DetailView):
+    model = Profile
+    template_name = 'profiles/profile-details.html'
+
+    def get_object(self, queryset: Optional[QuerySet]=None) -> QuerySet: # because we have only one profile
+        return get_profile()
+
+class ProfileDeleteView(DeleteView):
+    model = Profile
+    template_name = 'profiles/profile-delete.html'
+    success_url = reverse_lazy('home')
+
+    def get_object(self, queryset: Optional[QuerySet]=None) -> QuerySet: # because we have only one profile
+        return get_profile()
+
+
