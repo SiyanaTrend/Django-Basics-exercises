51528d81c12f5ecb46e35ddc52dbb6c5e1711c46
Siyana Trendafilova <bebovska@gmail.com>
2025-08-12 15:48:19 +0300

    feat: Add admin configuration

--- Exam-August2025/gameVerseApp/games/admin.py
+++ Exam-August2025/gameVerseApp/games/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from games.models import Game
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ['title', 'player']
